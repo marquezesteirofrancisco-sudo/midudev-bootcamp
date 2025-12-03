8bf5b4fcbf2c9666f58196bad703c2726ebfc7a2
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2025-12-03 17:28:19 +0100

    1.5: Información del Curso paso 5
    1.5: Información del Curso paso 5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,39 +1,42 @@
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const parts = [ {
-                    name: 'Fundamentals of React',
-                    exercises: 10
-                  },
-                  {
-                    name: 'Using props to pass data',
-                    exercises: 7
-                  },
-                  {
-                    name: 'State of a component',
-                    exercises: 14
-                  }
-            ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content  parts={parts} />
-      <Total parts={parts} />
+      <Content  parts={course} />
+      <Total parts={course} />
     </div>
   )
 }
 
 const Header = (props) => {
    return(
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
    )
 }
 
 const Content = (props) => {
    return(
       <div>
-        <Part parts={props}/>  
+        <Part parts={props}/> 
       </div>
    )
 }
@@ -41,7 +44,7 @@ const Content = (props) => {
 const Total = (props) => {
    return(
       <div>
-        <p>Number of exercises {props.parts[0].exercises  + props.parts[1].exercises + props.parts[2].exercises}</p>
+        <p>Number of exercises {props.parts.parts[0].exercises  + props.parts.parts[1].exercises + props.parts.parts[2].exercises}</p>
       </div>
    )
 }
@@ -51,16 +54,19 @@ const Part = (props) => {
    return(
       <div>
         {
+
           <div>
-            {props.parts.parts.map((curso, index) => (
+            {props.parts.parts.parts.map((curso, index) => (
               <div key={index}>
                 <p>{curso.name} {curso.exercises}</p>
               </div>
             ))}
           </div>
+
+
         }
       </div>
    )
-}
+} 
 
 export default App
