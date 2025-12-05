0714f310d15a1b98180be9fcf37e144240fd76eb
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2025-12-05 11:33:32 +0100

    1.14*: anecdotes, paso 3
    1.14*: anecdotes, paso 3

---
+++ Part1/anecdotes/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0,0])
+
+  const FindAnecdote = () => {
+  
+    const number = getRandomNumber(0,7);
+
+    setSelected(number) ;
+  }
+
+
+  const VoteAnecdote = () => {
+    setVotes(votes.map((vote, i) => i === selected ? vote + 1 : vote));
+  }
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  function ShowAnecdoteWithMostVotes() {
+    const maxVotes = Math.max(...votes);
+    const maxIndex = votes.indexOf(maxVotes)
+    return anecdotes[maxIndex];
+  }
+
+  return ( 
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {votes[selected]} votes</p>
+      
+      <button onClick={VoteAnecdote}>Vote</button>
+      <button onClick={FindAnecdote}>Next anecdote</button>
+      
+      <h1>Anecdote with most votes</h1>
+      <p>{ShowAnecdoteWithMostVotes()}</p>
+    </div>
+  )
+}
+
+export default App
